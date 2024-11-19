37270b766e53e27034491b41fa905a0e5adce820
gaurisanthosh02 <gaurisanthosh02@gmail.com>
2024-11-20 00:07:46 +0530

    first

---
+++ main.js
@@ -0,0 +1,19 @@
+// display in the screen function
+function displayContent(content){
+    result.value += content
+}
+
+//clear data
+function clearCalcScreen(){
+    result.value=""
+}
+
+// backspace
+function removeCalc(){
+    result.value = result.value.slice(0,-1)
+}
+
+//evaluation
+function calcout(){
+    result.value = eval(result.value)
+}
